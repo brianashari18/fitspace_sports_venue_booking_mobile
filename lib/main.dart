b8fef268308b3a7903097bd8ddef7081ca22fc45
Althaf Rizqullah <althaf8747@gmail.com>
2025-03-02 22:26:26 +0700

    firebase pub

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,19 @@ import 'package:fitspace_sports_venue_booking_mobile/screens/sign_in_screen.dart
 import 'package:fitspace_sports_venue_booking_mobile/screens/splash_screen.dart';
 import 'package:fitspace_sports_venue_booking_mobile/screens/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
