e84ce6339eda20149938406b9fed6fa31cd0e3df
Althaf Rizqullah <althaf8747@gmail.com>
2025-04-04 15:58:52 +0700

    model update

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,244 @@
+import 'package:fitspace_sports_venue_booking_mobile/services/user_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/user_model.dart';
+
+class AuthService {
+  final _baseUrl =
+      "http://${dotenv.env["HOST"]}:${dotenv.env["PORT"]}/api/users";
+  final UserService _userService = UserService();
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    try {
+      final responseLogin = await http.post(
+        Uri.parse('$_baseUrl/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      if (responseLogin.statusCode == 200) {
+        final body = json.decode(responseLogin.body);
+        final token = body['token'];
+
+        final responseUser = await http.get(
+          Uri.parse('$_baseUrl/current'),
+          headers: {'Authorization': 'Bearer $token'},
+        );
+
+        if (responseUser.statusCode == 200) {
+          final userBody = json.decode(responseUser.body);
+          final userData = userBody['data'];
+
+          final user = User(
+            id: userData['id'],
+            email: email,
+            token: token,
+            firstName: userData['firstName'],
+            lastName: userData['lastName'],
+          );
+          await _userService.saveUser(user);
+
+          return {'success': 'true', 'user': user};
+
+        } else if (responseUser.statusCode == 400) {
+          final body = json.decode(responseUser.body);
+          return {'success': false, 'error': body['errors']};
+
+        } else {
+          final body = json.decode(responseUser.body);
+          return {'success': false, 'error': body['errors']};
+        }
+
+      } else if (responseLogin.statusCode == 401) {
+        return {'success': false, 'error': 'Wrong username or password'};
+
+      } else {
+        return {
+          'success': false,
+          'error': 'Failed to login. Please try again.'
+        };
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> register(String email, String password, String confirmPassword) async {
+    try {
+      final responseRegister = await http.post(
+        Uri.parse('$_baseUrl/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'confirmPassword': confirmPassword
+        }),
+      );
+
+      if (responseRegister.statusCode == 200) {
+        final body = json.decode(responseRegister.body);
+        return {
+          'success': 'true',
+          'id': body['data']['id'],
+          'email': email,
+        };
+      } else if (responseRegister.statusCode == 400) {
+        final body = json.decode(responseRegister.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(responseRegister.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> forgotPassword(String email) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseUrl/forgot-password'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'email': email}),
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        return {
+          'success': 'true',
+          'message': body['message'],
+        };
+      } else if (response.statusCode == 400) {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> validateOTP(int otp) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseUrl/validate-otp'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'otp': otp}),
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        return {
+          'success': 'true',
+          'message': body['message'],
+        };
+      } else if (response.statusCode == 400) {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> resetPassword(
+      String newPassword, String confirmPassword, String email) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$_baseUrl/reset-password'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'newPassword': newPassword,
+          'confirmPassword': confirmPassword,
+          'email': email
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        return {
+          'success': 'true',
+          'message': body['message'],
+        };
+      } else if (response.statusCode == 400) {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> changeUsername(User user, String firstName, String lastName) async {
+    try {
+      final response = await http.patch(
+        Uri.parse('$_baseUrl/changeUsername/${user.id}'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': user.token
+        },
+        body: json.encode({
+          'firstName': firstName,
+          'lastName' : lastName
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        return {'success': 'true', 'data': body['data']};
+      } else if (response.statusCode == 400) {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+
+  Future<Map<String, dynamic>> changePassword(
+      User user, String password, String confirmPassword) async {
+    try {
+      final response = await http.patch(
+        Uri.parse('$_baseUrl/changePassword/${user.id}'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': user.token
+        },
+        body: json.encode({
+          'password' : password,
+          'confirmPassword' : confirmPassword
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        return {'success': 'true', 'data': body['data']};
+      } else if (response.statusCode == 400) {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      } else {
+        final body = json.decode(response.body);
+        return {'success': false, 'error': body['errors']};
+      }
+    } catch (e) {
+      return {'success': false, 'error': 'Error: $e'};
+    }
+  }
+}
+
+
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -7,16 +7,22 @@ class UserService {
     prefs.setInt('id', user.id);
     prefs.setString('email', user.email);
     prefs.setString('token', user.token);
-  }
+    prefs.setString('firstName', user.firstName);
+    if(user.lastName != null) {
+      prefs.setString('lastName', user.lastName!);
+    }
 
+  }
   Future<User?> getUser() async {
     final prefs = await SharedPreferences.getInstance();
     final id = prefs.getInt('id');
     final email = prefs.getString('email');
     final token = prefs.getString('token');
+    final firstName = prefs.getString('firstName');
+    final lastName = prefs.getString('lastName');
 
-    if (id != null && email != null && token != null) {
-      return User(id: id, email: email, token: token);
+    if (id != null && email != null && token != null && firstName != null && lastName != null) {
+      return User(id: id, email: email, token: token, firstName: firstName, lastName: lastName);
     }
 
     return null;
